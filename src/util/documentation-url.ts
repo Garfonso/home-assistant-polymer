6a39f1626e3a094c79e8e6358247451ecf58345f
Garfonso <garfonso@mobo.info>
2023-11-23 13:47:33 +0100

    fix 404 error on specific documentation urls.. hm.

--- src/util/documentation-url.ts
+++ src/util/documentation-url.ts
@@ -4,5 +4,5 @@ export const documentationUrl = (hass: HomeAssistant, path: string) => {
   // IoB Help Link:
   return `https://www.iobroker.net/#${
     hass!.language ? hass!.language.split("-")[0] || "en" : "en"
-  }/adapters/adapterref/iobroker.lovelace/README.md`;
+  }/adapters/adapterref/iobroker.lovelace/README.md#`;
 };
